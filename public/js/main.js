12ab7b7fff8386a1dc81e96027b5910602877b64
FrancoisPala <francois.pala@epitech.eu>
2016-07-09 03:42:22 -0400

    .

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,47 @@ app.config(['$routeProvider', function ($routeProvider) {
  * Controls all other Pages
  */
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
+  //console.log("Page Controller reporting for duty.");
 
 
 });
+
+/**
+ * Fcts
+ */
+
+function sendSignIn() {
+  let name = $("#signinName").val();
+  console.log("name = " + name);
+  let pw = $("#signinPw").val();
+  $.post("http://ec2-50-112-40-35.us-west-2.compute.amazonaws.com:12345/connect", {user:name, password:pw}, function (data, status) {
+    //console.log(status);
+    if (status == "success"){
+      let obj = JSON.parse(JSON.stringify(data));
+      console.log(obj);
+
+      let id = obj['id'];
+      let username = obj['login'];
+
+      let ai = $("#account-info");
+      ai.append("<p>My account<br><br>Name: " + username + "<br>id: " + id + "<br>email: netsil@netsil.com</p>");
+
+    }
+    else
+      console.log("request failed");
+  });
+
+}
+
+function sendSignUp() {
+  let name = $("#signupName").val();
+  let email = $("#email").val();
+  let pw = $("#signupPw").val();
+  $.post("http://ec2-50-112-40-35.us-west-2.compute.amazonaws.com:12345/register", {username:name, password:pw}, function (data, status) {
+    if (status == "success"){
+      console.log("success, data is: " + data);
+    }
+    else
+      console.log("request failed");
+  });
+}
